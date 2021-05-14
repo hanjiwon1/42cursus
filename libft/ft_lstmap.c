f67cde6dcec89206f7e05718cbb1bf84e798f2f0
Jiwon Han <jiwhan@c8r1s8.42seoul.kr>
2021-05-14 21:04:07 +0900

    modified ft_lstmap

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jiwhan <jiwhan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 18:27:59 by jiwhan            #+#    #+#             */
-/*   Updated: 2021/05/14 18:43:54 by jiwhan           ###   ########.fr       */
+/*   Updated: 2021/05/14 18:46:07 by jiwhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	{
 		if (!(new_node = ft_lstnew(f(lst->content))))
 		{
-			ft_lstclear(new_head, del);
+			ft_lstclear(&new_head, del);
 			return (NULL);
 		}
-		ft_lstadd_back(new_head, new_node);
+		ft_lstadd_back(&new_head, new_node);
 		lst = lst->next;
 	}
 	return (new_head);
